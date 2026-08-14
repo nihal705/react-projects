5692655d849cfb547f41d9fc1505e2e1fff0148f
Nihal <nihalmohammad705@gmail.com>
2026-08-14 20:33:26 +0530

    feat: initialize react-projects with calculator version 2

---
+++ calculator-version-2/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+import ButtonsContainer from './components/ButtonsContainer'
+import Display from './components/Display'
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+  
+  const onButtonClick = (buttonValue) => {
+    if(buttonValue === 'C'){
+      setCalVal("");
+    } else if (buttonValue === '=') {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonValue;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal} />
+      <ButtonsContainer onButtonClick={onButtonClick} />
+    </div>
+  )
+}
+
+export default App
